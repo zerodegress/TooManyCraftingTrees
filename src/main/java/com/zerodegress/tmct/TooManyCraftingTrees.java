dd39bca1d154c77ea32d98253c6255aa8b3c633d
zerodegress <zerodegress@outlook.com>
2026-05-29 14:09:19 +0800

    chore: init project

---
+++ src/main/java/com/zerodegress/tmct/TooManyCraftingTrees.java
@@ -0,0 +1,20 @@
+package com.zerodegress.tmct;
+
+import org.slf4j.Logger;
+
+import com.mojang.logging.LogUtils;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.neoforge.common.NeoForge;
+
+@Mod(TooManyCraftingTrees.MODID)
+public class TooManyCraftingTrees {
+    public static final String MODID = "too_many_crafting_trees";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+    public TooManyCraftingTrees(IEventBus modEventBus, ModContainer modContainer) {
+        NeoForge.EVENT_BUS.register(this);
+    }
+}
